15eb8ab2d008eec2a813b469f0baeb3ba84c59d4
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-11-28 16:58:21 +0300

    add algo v2

---
+++ src/utils/v2.ts
@@ -0,0 +1,48 @@
+export const createProductionStepExecution = () => {
+  const productionStepExecutions = [];
+
+  for (const productionItem of productionItems) {
+    for (const section of productionItem.recipe.sections) {
+      for (const productionStepObj of section.productionSteps) {
+        const productionStep = productionStepObj.step;
+        // console.log("productionStep", productionStep.name);
+        const productionStepExecution: any = {};
+        productionStepExecution.productionStep = { step: productionStep };
+        productionStepExecution.productionItems = productionItems;
+        const priorSteps = [];
+
+        if (productionStep.stepComponents) {
+          for (const stepComponent of productionStep.stepComponents) {
+            if (stepComponent.priorSteps) {
+              const productionStep = stepComponent.priorSteps;
+              // console.log("productionStep priors", productionStep.name);
+
+              const priorProductionStepExecution: any = {};
+              priorProductionStepExecution.productionStep = {
+                step: productionStep
+              };
+              // .save()
+              const newPriorProductionStepExecution = priorProductionStepExecution;
+              priorSteps.push(newPriorProductionStepExecution);
+              // newPriorProductionStepExecution.priorSteps = priorSteps
+              // productionStepExecutions.push(newPriorProductionStepExecution);
+            }
+          }
+        }
+
+        productionStepExecution.priorSteps = priorSteps;
+
+        if (priorSteps.length > 0) {
+          productionStepExecution.status = "LOCKED";
+        } else {
+          productionStepExecution.status = "TODO";
+        }
+        // .save()
+        // await productionStepExecution.save()
+        productionStepExecutions.push(productionStepExecution);
+      }
+    }
+  }
+
+  return productionStepExecutions;
+};
