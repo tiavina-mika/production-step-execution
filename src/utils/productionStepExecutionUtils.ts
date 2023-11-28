c669304cd49c17035d2c52d513e666c6e97c9b05
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-11-28 17:20:02 +0300

    algo v3

--- src/utils/productionStepExecutionUtils.ts
+++ src/utils/productionStepExecutionUtils.ts
@@ -1,48 +1,71 @@
 import { productionItems } from "../data/productionItem";
 
-export const createProductionStepExecution = () => {
+const getProductionStepExecutions = (productionSteps = []): any => {
   const productionStepExecutions = [];
+
+  for (const productionStepObj of productionSteps) {
+    const productionStep = (productionStepObj as any).step ?? productionStepObj;
+    // console.log("productionStep",
+    // productionStep.name, "-",
+    // productionStep);
+
+    if (productionStep.productionSteps) {
+      console.log("productionStep", productionStep.name, "-", productionStep);
+
+      productionStepExecutions.push(
+        ...getProductionStepExecutions(productionStep.productionSteps)
+      );
+    } else {
+      console.log("productionStep 2", productionStep.name, "-", productionStep);
+
+      const productionStepExecution: any = {};
+      productionStepExecution.productionStep = { step: productionStep };
+      productionStepExecution.productionItems = productionItems;
+      const priorSteps = [];
+
+      if (productionStep.stepComponents) {
+        for (const stepComponent of productionStep.stepComponents) {
+          if (stepComponent.priorSteps) {
+            const productionStep = stepComponent.priorSteps;
+            // console.log("productionStep priors", productionStep.name);
+
+            const priorProductionStepExecution: any = {};
+            priorProductionStepExecution.productionStep = {
+              step: productionStep
+            };
+            // .save()
+            const newPriorProductionStepExecution = priorProductionStepExecution;
+            priorSteps.push(newPriorProductionStepExecution);
+            // newPriorProductionStepExecution.priorSteps = priorSteps
+            // productionStepExecutions.push(newPriorProductionStepExecution);
+          }
+        }
+      }
+
+      productionStepExecution.priorSteps = priorSteps;
+
+      if (priorSteps.length > 0) {
+        productionStepExecution.status = "LOCKED";
+      } else {
+        productionStepExecution.status = "TODO";
+      }
+      // .save()
+      // await productionStepExecution.save()
+      productionStepExecutions.push(productionStepExecution);
+    }
+  }
+
+  return productionStepExecutions;
+};
+
+export const createProductionStepExecution = () => {
+  let productionStepExecutions = [];
 
   for (const productionItem of productionItems) {
     for (const section of productionItem.recipe.sections) {
-      for (const productionStepObj of section.productionSteps) {
-        const productionStep = productionStepObj.step;
-        // console.log("productionStep", productionStep.name);
-        const productionStepExecution: any = {};
-        productionStepExecution.productionStep = { step: productionStep };
-        productionStepExecution.productionItems = productionItems;
-        const priorSteps = [];
-
-        if (productionStep.stepComponents) {
-          for (const stepComponent of productionStep.stepComponents) {
-            if (stepComponent.priorSteps) {
-              const productionStep = stepComponent.priorSteps;
-              // console.log("productionStep priors", productionStep.name);
-
-              const priorProductionStepExecution: any = {};
-              priorProductionStepExecution.productionStep = {
-                step: productionStep
-              };
-              // .save()
-              const newPriorProductionStepExecution = priorProductionStepExecution;
-              priorSteps.push(newPriorProductionStepExecution);
-              // newPriorProductionStepExecution.priorSteps = priorSteps
-              // productionStepExecutions.push(newPriorProductionStepExecution);
-            }
-          }
-        }
-
-        productionStepExecution.priorSteps = priorSteps;
-
-        if (priorSteps.length > 0) {
-          productionStepExecution.status = "LOCKED";
-        } else {
-          productionStepExecution.status = "TODO";
-        }
-        // .save()
-        // await productionStepExecution.save()
-        productionStepExecutions.push(productionStepExecution);
-      }
+      productionStepExecutions = getProductionStepExecutions(
+        (section as any).productionSteps
+      );
     }
   }
 
--- src/utils/v2.ts
+++ src/utils/v2.ts
@@ -1,3 +1,5 @@
+import { productionItems } from "../data/productionItem";
+
 export const createProductionStepExecution = () => {
   const productionStepExecutions = [];
 
@@ -13,6 +15,58 @@ export const createProductionStepExecution = () => {
 
         if (productionStep.stepComponents) {
           for (const stepComponent of productionStep.stepComponents) {
+            if (stepComponent.priorSteps) {
+              const productionStep = stepComponent.priorSteps;
+              // console.log("productionStep priors", productionStep.name);
+
+              const priorProductionStepExecution: any = {};
+              priorProductionStepExecution.productionStep = {
+                step: productionStep
+              };
+              // .save()
+              const newPriorProductionStepExecution = priorProductionStepExecution;
+              priorSteps.push(newPriorProductionStepExecution);
+              // newPriorProductionStepExecution.priorSteps = priorSteps
+              // productionStepExecutions.push(newPriorProductionStepExecution);
+            }
+          }
+        }
+
+        productionStepExecution.priorSteps = priorSteps;
+
+        if (priorSteps.length > 0) {
+          productionStepExecution.status = "LOCKED";
+        } else {
+          productionStepExecution.status = "TODO";
+        }
+        // .save()
+        // await productionStepExecution.save()
+        productionStepExecutions.push(productionStepExecution);
+      }
+    }
+  }
+
+  return productionStepExecutions;
+};
+
+export const createProductionStepExecution3 = () => {
+  const productionStepExecutions = [];
+
+  for (const productionItem of productionItems) {
+    for (const section of productionItem.recipe.sections) {
+      for (const productionStepObj of section.productionSteps) {
+        const productionStep = productionStepObj.step ?? productionStepObj;
+        // console.log("productionStep",
+        // productionStep.name, "-",
+        // productionStep);
+
+        const productionStepExecution: any = {};
+        productionStepExecution.productionStep = { step: productionStep };
+        productionStepExecution.productionItems = productionItems;
+        const priorSteps = [];
+
+        if ((productionStep as any).stepComponents) {
+          for (const stepComponent of (productionStep as any).stepComponents) {
             if (stepComponent.priorSteps) {
               const productionStep = stepComponent.priorSteps;
               // console.log("productionStep priors", productionStep.name);
