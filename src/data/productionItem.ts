694f0f105a51458e96a4e0191834cb47478c6cf1
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-12-01 10:39:12 +0300

    fix weight

--- src/data/productionItem.ts
+++ src/data/productionItem.ts
@@ -1,14 +1,14 @@
 import { recipe, recipe2 } from "./recipe";
 
 export const productionItems = [
-  // {
-  //   objectId: "pI01",
-  //   recipe,
-  //   expectedProduction: 100,
-  // },
   {
-    objectId: "pI02",
-    recipe: recipe2,
-    expectedProduction: 200
+    objectId: "pI01",
+    recipe,
+    expectedProduction: 100
   }
+  // {
+  //   objectId: "pI02",
+  //   recipe: recipe2,
+  //   expectedProduction: 200
+  // }
 ];
