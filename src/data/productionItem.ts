178cab0434dfe0b5869592cee8c7b2ddd1732d99
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-12-14 22:35:34 +0300

    clean code

--- src/data/productionItem.ts
+++ src/data/productionItem.ts
@@ -5,18 +5,18 @@ export const productionItems = [
     objectId: "pI01",
     recipe,
     expectedProduction: 100
-  }
+  },
   // {
   //   objectId: "pI02",
   //   recipe,
   //   expectedProduction: 200
   // },
 
-  // {
-  //   objectId: "pI02",
-  //   recipe: recipe2,
-  //   expectedProduction: 800
-  // },
+  {
+    objectId: "pI02",
+    recipe: recipe2,
+    expectedProduction: 800
+  },
   // {
   //   objectId: "3",
   //   recipe: recipe2,
